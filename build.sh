cc7fa234fde97cc71f144bb24c5f2e9ceee013db
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-02-23 14:02:59 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y wget tar libncurses-dev bison flex libssl-dev libelf-dev bc zstd unzip rsync
+
 wget -q -i kernel-url -O kernel.tar.xz
 mkdir -p kext
 tar -xf kernel.tar.xz -C kext --strip-components=1
