7e02944d99b0619a42418e8a402176ce3b9f18ff
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-08 11:56:39 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 sudo apt-get update
-sudo apt-get install -y wget tar libncurses-dev bison flex libssl-dev libelf-dev bc zstd unzip rsync clang llvm lld binutils
+sudo apt-get install -y wget tar libncurses-dev bison flex libssl-dev libelf-dev bc zstd unzip  binutils
 
 wget -q -i kernel-url -O kernel.tar.xz
 mkdir -p kext
 tar -xf kernel.tar.xz -C kext --strip-components=1
-rsync -av .config kext/
-rsync -av fw kext/
+cp  .config kext/
+cp -r fw kext/
 cd kext
-make  olddefconfig LLVM=1
-make -j$(nproc) LLVM=1
+make  olddefconfig 
+make -j$(nproc) 
 
