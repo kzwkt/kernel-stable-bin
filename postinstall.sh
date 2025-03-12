bb085f7f45920a894f3cc7ee8e3cc853d0385ae0
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-12 07:59:23 +0545

    Create postinstall.sh

---
+++ postinstall.sh
@@ -0,0 +1,23 @@
+
+emerge-webrsync
+
+emerge app-portage/cpuid2cpuflags
+echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
+
+emerge --ask --verbose --update --deep --newuse @world
+
+ln -s /usr/share/zoneinfo/Asia/Kathmandu /etc/localtime
+
+
+nano /etc/locale.gen
+en_US.UTF-8 UTF-8
+locale-gen
+
+emerge -a gentoolkit eix  eselect-repository  dev-vcs/git  
+rm -r /var/db/repos/gentoo
+eselect repository enable gentoo
+emerge --sync
+
+
+emerge -a iwd
+
