b64cedb7e2215a1c77f76d633ca0ce3da3175e19
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-22 13:46:21 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
-sudo apt-get update
+ver=6.13.7
+echo $ver > ver
+kernel-url="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-$ver.tar.xz"
+
+pt-get update
 sudo apt-get install -y wget tar libncurses-dev bison flex libssl-dev libelf-dev bc zstd unzip  binutils
 
-wget -q -i kernel-url -O kernel.tar.xz
+wget  $kernel-url -O kernel.tar.xz
 mkdir -p kext
 tar -xf kernel.tar.xz -C kext --strip-components=1
 cp  .config kext/
