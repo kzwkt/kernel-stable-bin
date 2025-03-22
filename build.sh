a0a10d11a3f73a2d59918dbedc1c94338b2a0e8b
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-22 14:02:21 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 ver=6.13.7
 echo $ver > ver
-kernel-url="https://cdn.kernel.org/pub/linux/kernel/v${ver%%.*}.x/linux-$ver.tar.xz"
-hardened-patch="https://github.com/anthraxx/linux-hardened/releases/download/v$ver-hardened1/linux-hardened-v$ver-hardened1.patch"
+kernel_url="https://cdn.kernel.org/pub/linux/kernel/v${ver%%.*}.x/linux-$ver.tar.xz"
+hardened_patch="https://github.com/anthraxx/linux-hardened/releases/download/v$ver-hardened1/linux-hardened-v$ver-hardened1.patch"
 
 
 pt-get update
 sudo apt-get install -y wget tar libncurses-dev bison flex libssl-dev libelf-dev bc zstd unzip  binutils
 
-wget  $kernel-url -O kernel.tar.xz
+wget  $kernel_url -O kernel.tar.xz
 mkdir -p kext
 tar -xf kernel.tar.xz -C kext --strip-components=1
 cp  .config kext/
 cp -r fw kext/
 cd kext
-wget $hardened-patch
+wget $hardened_patch
 patch -Np1 < linux-hardened-v$ver-hardened1.patch
 make  olddefconfig 
 make -j$(nproc) 
